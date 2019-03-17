0e884d401d90f2f39941bd38fc30208a56185234
wuyu1998 <wuyu1998@sina.com>
2019-03-17 09:30:14 +0800

    2.3 快速设计对话框 02

--- C++ GUI Programming with Qt 4/chap02/gotocell/main.cpp
+++ C++ GUI Programming with Qt 4/chap02/gotocell/main.cpp
@@ -1,16 +1,11 @@
 #include <QApplication>
-#include <QDialog>
 
-#include "ui_gotocelldialog.h"
+#include "gotocelldialog.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-
-    Ui::GoToCellDialog ui;
-    QDialog *dialog = new QDialog;
-    ui.setupUi(dialog);
+    GoToCellDialog *dialog = new GoToCellDialog;
     dialog->show();
-
     return app.exec();
 }
